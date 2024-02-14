4fdb117ed93a3a38a90c8ffdc86a1f45cd58ffff
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-02-14 14:42:37 +0700

    Init commit Add Header Add Baners (huge, small)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        saldo: '#3E5DA7',
+      }
+    },
+  },
+  plugins: [],
+}
