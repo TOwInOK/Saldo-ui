8e04ddc96161522b9d18aec72b74ccd1e1cea2d2
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-02-27 12:09:05 +0700

    add gramota setcion change space between buttons into card sections

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,8 @@ export default defineConfig({
               extend: {
                 colors: {
                   "saldo": "#3E5DA7",
+                  "saldo-white": "#F2F3F5",
+                  "saldo-50": "#F8F8F8",
                   "saldo-300": "#9EAED3",
                   "saldo-100": "#E6EDFF",
                   "saldo-600": "#1D2848",
