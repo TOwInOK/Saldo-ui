29f144979644894f1b312e12eb449e9c1911bae9
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-02-28 10:08:27 +0700

    fix tailwindcss content roads

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./index.pug",
+  "./src/**/*.{js,pug}"],
+  theme: {
+    extend: {
+      colors: {
+        "saldo": "#3E5DA7",
+        "saldo-white": "#F2F3F5",
+        "saldo-50": "#F8F8F8",
+        "saldo-300": "#9EAED3",
+        "saldo-100": "#E6EDFF",
+        "saldo-600": "#1D2848",
+      },
+      screens: {
+        'vsm': '375px',
+      }
+    },
+  },
+  plugins: [],
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
 import vitePugPlugin from "vite-plugin-pug-transformer";
-import tailwindcss from "tailwindcss";
 
 const option = {
   basedir: "./**/*",
@@ -20,34 +18,6 @@ export default defineConfig({
       input: {
         main: "./index.html",
       },
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({}),
-        tailwindcss({
-          config: {
-            content: ["./index.html", "./src/**/*.{js,html,pug}"],
-            theme: {
-              extend: {
-                colors: {
-                  "saldo": "#3E5DA7",
-                  "saldo-white": "#F2F3F5",
-                  "saldo-50": "#F8F8F8",
-                  "saldo-300": "#9EAED3",
-                  "saldo-100": "#E6EDFF",
-                  "saldo-600": "#1D2848",
-                },
-                screens: {
-                  'vsm': '375px',
-                }
-              },
-            },
-            plugins: [],
-          },
-        }),
-      ],
     },
   },
 });
