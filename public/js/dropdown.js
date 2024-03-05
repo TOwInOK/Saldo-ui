7e3c13a6b065ef183a010c5c1bee8fb4a6a3b6fe
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-03-05 16:34:18 +0700

    add class anotation for svg: arrow-icon-90 now widget "Производитель" is dropdown menu

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,30 +1,38 @@
-//Dropdown all
 document.addEventListener('DOMContentLoaded', function () {
   const dropdownButtons = document.querySelectorAll('.dropdown-button');
 
   dropdownButtons.forEach(function (button) {
-  const arrowIcon = button.querySelector('.arrow-icon');
+    const arrowIcon = button.querySelector('.arrow-icon');
+    const arrowIcon_90 = button.querySelector('.arrow-icon-90');
+    const dropdownMenu = button.nextElementSibling; // Получаем следующий элемент (div.dropdown-menu)
 
-  button.addEventListener('click', function () {
-  const dropdownMenu = this.nextElementSibling; // Получаем следующий элемент (div.dropdown-menu)
+    button.addEventListener('click', function () {
+      // Переключаем видимость меню
+      dropdownMenu.classList.toggle('hidden');
 
-  // Переключаем видимость меню
-  dropdownMenu.classList.toggle('hidden');
-
-  // Поворот стрелки на 180 градусов при открытии, иначе 0 градусов
-  arrowIcon.style.transform = dropdownMenu.classList.contains('hidden') ? 'rotate(0deg)' : 'rotate(180deg)';
-});
-});
+      // Поворот стрелки в зависимости от состояния меню
+      if (typeof arrowIcon !== 'undefined' && arrowIcon !== null) {
+        // arrowIcon существует
+        arrowIcon.style.transform = dropdownMenu.classList.contains('hidden') ? 'rotate(0deg)' : 'rotate(180deg)';
+      } else if (typeof arrowIcon_90 !== 'undefined' && arrowIcon_90 !== null) {
+        // arrowIcon_90 существует
+        arrowIcon_90.style.transform = dropdownMenu.classList.contains('hidden') ? 'rotate(90deg)' : 'rotate(270deg)';
+      }
+    });
+  });
 
   // Закрытие меню при клике вне него
   document.addEventListener('click', function (event) {
-  dropdownButtons.forEach(function (button) {
-  const dropdownMenu = button.nextElementSibling;
+    dropdownButtons.forEach(function (button) {
+      const dropdownMenu = button.nextElementSibling;
 
-  if (!button.contains(event.target) && !dropdownMenu.contains(event.target)) {
-  dropdownMenu.classList.add('hidden');
-  button.querySelector('.arrow-icon').style.transform = 'rotate(0deg)';
-}
-});
-});
+      if (typeof arrowIcon !== 'undefined' && arrowIcon !== null) {
+        // arrowIcon существует
+        arrowIcon.style.transform = dropdownMenu.classList.contains('hidden') ? 'rotate(0deg)' : 'rotate(180deg)';
+      } else if (typeof arrowIcon_90 !== 'undefined' && arrowIcon_90 !== null) {
+        // arrowIcon_90 существует
+        arrowIcon_90.style.transform = dropdownMenu.classList.contains('hidden') ? 'rotate(90deg)' : 'rotate(180deg)';
+      }
+    });
+  });
 });
